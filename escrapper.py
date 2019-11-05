53471e02313a181d42c63ee5b2aaefbc24b3a742
gink3 <telltaylor@yahoo.com>
2019-11-05 09:55:49 -0600

    Added comments

--- escrapper.py
+++ escrapper.py
@@ -3,8 +3,8 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
-url = "http://kunalsdatabase.com/ebooks/2016/"
-response = requests.get(url)
+url = "http://kunalsdatabase.com/ebooks/2016/"    #sets url
+response = requests.get(url)                      #gets the html page 
 
 soup = BeautifulSoup(response.text, "html.parser")
 
@@ -13,14 +13,16 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 
 # http://kunalsdatabase.com/ebooks/2016/1001%20People%20Who%20Made%20America%20-%20Alan%20Axelrod.epub
+# 
 # 1001%20People%20Who%20Made%20America%20-%20Alan%20Axelrod.epub
-
+# Turns this link into
+# 1001PeopleWhoMadeAmerica-AlanAxelrod.epub
+# for the file name
 
 for i in range(6,len(soup.findAll('a'))+1): #'a' tags are for links
-    one_a_tag = soup.findAll('a')[i]
-    link = one_a_tag['href']
-    name = link.replace("%20","")
-    #name = name.replace("-","")
+    one_a_tag = soup.findAll('a')[i]        #Grabs 1 tag at a time
+    link = one_a_tag['href']                #set the link to the value of the href
+    name = link.replace("%20","")           #Makes the file names readable
     print(name)
     download_url = 'http://kunalsdatabase.com/ebooks/2016/'+ link
     urllib.request.urlretrieve(download_url,name) 
