096c63f99da0acfc0e6233636d40c2b7c5a97093
gink3 <telltaylor@yahoo.com>
2019-11-05 08:51:27 -0600

    Made file names readable

--- escrapper.py
+++ escrapper.py
@@ -13,13 +13,15 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 
 # http://kunalsdatabase.com/ebooks/2016/1001%20People%20Who%20Made%20America%20-%20Alan%20Axelrod.epub
+# 1001%20People%20Who%20Made%20America%20-%20Alan%20Axelrod.epub
 
 
 for i in range(6,len(soup.findAll('a'))+1): #'a' tags are for links
     one_a_tag = soup.findAll('a')[i]
     link = one_a_tag['href']
-    name = link.replace("%","_")
+    name = link.replace("%20","")
+    #name = name.replace("-","")
     print(name)
     download_url = 'http://kunalsdatabase.com/ebooks/2016/'+ link
     urllib.request.urlretrieve(download_url,name) 
-    #time.sleep(1) #pause the code for a sec
+    time.sleep(1) #pause the code for a sec
